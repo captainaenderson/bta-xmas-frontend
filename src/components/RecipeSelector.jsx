9a8906fe6dc8860dba127e832afd7fc6dcc73188
captainaenderson <annkathrin.wohlrab@gmail.com>
2023-12-15 14:16:39 +0100

    breite anleitung angepasst

--- src/components/RecipeSelector.jsx
+++ src/components/RecipeSelector.jsx
@@ -24,39 +24,38 @@ const recipes = [
     name: "Vegan",
     description: "Vanilla Crescents",
     zutaten: [
-        "300g flour (type 405 | 550)",
-        "70g sugar",
-        "250g margarine",
-        "100 g ground almonds",
-        "2 packets vanilla sugar",
-        "1 pinch of salt",
-      ],
-  
-      anleitung: [
-        "1. Cut cold, vegan butter into coarse cubes. In a large bowl, mix flour, ground almonds, sugar, vanilla sugar, salt, and butter cubes, and knead into a smooth dough. Cover or wrap in foil and chill for 1 hour.",
-        "2. Preheat the oven to 175 degrees Celsius (347 degrees Fahrenheit) using top and bottom heat. Line a baking tray with parchment paper. Quarter the dough and put all but one piece back in the fridge. Shape each piece of dough into a roll about 1.5 cm thick and cut off slices about 1 cm wide. Shape into crescents using the palms of your hands. Repeat with the remaining pieces until all the dough is used up.",
-        "3. Place the Vanilla Crescents on the baking tray and bake in the oven for about 12-15 minutes until they start to turn golden brown at the tips. In the meantime, sift powdered sugar into a bowl and mix with sugar and vanilla sugar.",
-        "4. Take the Vanilla Crescents out of the oven, let them cool for 2-3 minutes, and while still warm, roll them in the powdered sugar mixture. Let them cool completely.",
-      ],
+      "300g flour (type 405 | 550)",
+      "70g sugar",
+      "250g margarine",
+      "100 g ground almonds",
+      "2 packets vanilla sugar",
+      "1 pinch of salt",
+    ],
+
+    anleitung: [
+      "1. Cut cold, vegan butter into coarse cubes. In a large bowl, mix flour, ground almonds, sugar, vanilla sugar, salt, and butter cubes, and knead into a smooth dough. Cover or wrap in foil and chill for 1 hour.",
+      "2. Preheat the oven to 175 degrees Celsius (347 degrees Fahrenheit) using top and bottom heat. Line a baking tray with parchment paper. Quarter the dough and put all but one piece back in the fridge. Shape each piece of dough into a roll about 1.5 cm thick and cut off slices about 1 cm wide. Shape into crescents using the palms of your hands. Repeat with the remaining pieces until all the dough is used up.",
+      "3. Place the Vanilla Crescents on the baking tray and bake in the oven for about 12-15 minutes until they start to turn golden brown at the tips. In the meantime, sift powdered sugar into a bowl and mix with sugar and vanilla sugar.",
+      "4. Take the Vanilla Crescents out of the oven, let them cool for 2-3 minutes, and while still warm, roll them in the powdered sugar mixture. Let them cool completely.",
+    ],
   },
   {
     name: "Gluten Free",
     description: " Oat Macaroons with Rosehip Jam",
     zutaten: [
-        "125 g oat flakes",
-        "2 egg whites (size M)",
-        "2 tablespoons sugar",
-        "180 g rosehip jam",
-        "50 g ground almonds (without skin)",
-      ],
-  
-      anleitung: [
-        "1. Preheat the oven (electric oven: 150 °C/ convection: 125 °C). Line a baking tray with parchment paper. Briefly toast the oat flakes in a pan without fat. Remove immediately and let cool.",
-        "2. Whisk the egg whites with a hand mixer on the highest setting until stiff. Gradually stir in the sugar and 100 g of the jam. Gently fold in the almonds and oat flakes.",
-        "3. Spoon small mounds of the macaroon mixture onto the baking tray using two teaspoons. Then, press a small indentation into the macaroons using the round end of a spoon handle. Bake the oat flake macaroons in the hot oven for about 30 minutes.",
-        "4. While still hot, fill the oat flake macaroons with 80 g of jam and let them cool on the baking tray for about 1 hour.",
+      "125 g oat flakes",
+      "2 egg whites (size M)",
+      "2 tablespoons sugar",
+      "180 g rosehip jam",
+      "50 g ground almonds (without skin)",
+    ],
 
-      ],
+    anleitung: [
+      "1. Preheat the oven (electric oven: 150 °C/ convection: 125 °C). Line a baking tray with parchment paper. Briefly toast the oat flakes in a pan without fat. Remove immediately and let cool.",
+      "2. Whisk the egg whites with a hand mixer on the highest setting until stiff. Gradually stir in the sugar and 100 g of the jam. Gently fold in the almonds and oat flakes.",
+      "3. Spoon small mounds of the macaroon mixture onto the baking tray using two teaspoons. Then, press a small indentation into the macaroons using the round end of a spoon handle. Bake the oat flake macaroons in the hot oven for about 30 minutes.",
+      "4. While still hot, fill the oat flake macaroons with 80 g of jam and let them cool on the baking tray for about 1 hour.",
+    ],
   },
 ];
 
@@ -64,7 +63,7 @@ const RecipeSelector = () => {
   const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   return (
-    <div className="pl-10 pr-10">
+    <div className="pl-10 pr-10 max-w-7xl mx-auto" >
       <h2 className="text-2xl font-mono font-bold uppercase text-white text-center mt-20">
         Pick your recipe 🎲
       </h2>
@@ -84,20 +83,22 @@ const RecipeSelector = () => {
         ))}
       </div>
       {selectedRecipe && (
-        <div className="flex flex-col md:flex-row justify-center items-center mt-5 mb-10">
+        <div className="flex flex-col md:flex-row justify-center  mt-5 mb-10">
           <div className="md:w-1/2 md:mr-4">
-          <h2 className="text-2xl font-mono font-bold text-white text-center uppercase mb-10">
-  {selectedRecipe.name}
-  {selectedRecipe.description && ` - ${selectedRecipe.description}`}
-</h2>
-
+            <h2 className="text-2xl font-mono font-bold text-white text-center uppercase mb-10">
+              {selectedRecipe.name}
+              {selectedRecipe.description && ` - ${selectedRecipe.description}`}
+            </h2>
 
             <h2 className="text-white uppercase font-mono font-bold mt-5 underline">
               Ingredients
             </h2>
             <ul className="list-none">
               {selectedRecipe.zutaten.map((zutat, index) => (
-                <li key={index} className="text-white mt-5 mb-5 relative pl-6 font-mono text-sm">
+                <li
+                  key={index}
+                  className="text-white mt-5 mb-5 relative pl-6 font-mono text-sm"
+                >
                   <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-black border border-white"></div>
                   {zutat}
                 </li>

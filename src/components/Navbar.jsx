50fbdb96bfff02e4b261ef51c81a204a29af7db9
captainaenderson <annkathrin.wohlrab@gmail.com>
2023-12-21 23:52:28 +0100

    navi gelb

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 function Navbar() {
 	return (
-	  <nav className="font-mono flex justify-between items-center mt-5 px-4">
+	  <nav className="font-mono flex justify-between items-center mt-5 px-4 mx-auto max-w-2xl">
 		<div className="flex-1"></div> {/* Linker Platzhalter für Ausgleich */}
 		
-		<ul className="flex items-center text-white">
+		<ul className="flex items-center text-white ">
 		  <li className="mx-2">
 			<a href="#rezepte" className="nav-link">Merry</a>
 		  </li>
@@ -15,7 +15,7 @@ function Navbar() {
 		  </li>
 		</ul>
 		
-		<ul className="flex items-center text-white flex-1 justify-end md:flex-row flex-col">
+		<ul className="flex items-center text-yellow-200 flex-1 justify-end md:flex-row flex-col">
 		  <li className="mx-2 nav-link md:mb-0 mb-2">
 			<a href="/">EN</a>
 		  </li>
